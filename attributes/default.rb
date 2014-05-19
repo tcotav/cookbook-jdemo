83a0d893b3fe00584b4134b8bbf1448cee371845
James Francis <tcotav@gmail.com>
2014-05-19 12:55:52 -0700

    wrote tests, did not test...  fixed

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,4 @@ default['java']['jdk_version'] = '7'
 
 # the web location of our
 # so I put a fake version tag in there suggesting that this particular file was a special flower
-default['jdemo']['war']['base_source_url']='https://github.com/tcotav/punter/blob/master/'
-default['jdemo']['war']['war_file']='punter.war.0101003?raw=true'
+default['jdemo']['war']['src_url']='https://github.com/tcotav/punter/raw/master/punter.war'
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,40 +10,22 @@
 include_recipe 'apt'
 include_recipe 'tomcat'
 
-
-
-#default['jdemo']['war']['base_source_url']='https://github.com/tcotav/punter/blob/master/'
-#default['jdemo']['war']['war_file']='punter.war.0101003?raw=true'
-
 # retrieve and untar package
-src_url="#{node['jdemo']['war']['base_src_url']}/#{node['jdemo']['war']['war_file']}"
-src_filepath=Chef::Config[:file_cache_path]
-src_filename=node['jdemo']['war']['war_file'].split('?')[0]
-
-local_file="#{src_filepath}/#{src_filename}"
-
-## if web browser supports ETag then remote_file is idempotent
-## else we'll have to check file/hash
-remote_file local_file do
-  source src_url
-  notifies :stop,  "service tomcat", :immediately
-  notifies :create,  "file /var/lib/tomcat6/webapps/punter.war", :immediately
-  not_if { ::File.exists? local_file }
-end
+src_url= node['jdemo']['war']['src_url']
 
 service "tomcat" do
   action :nothing
 end
 
-file "/var/lib/tomcat6/webapps/punter.war"  do
+# shasum -a 256 punter.war   -- to get checksum
+remote_file "punter.war in repo" do
+  path "/var/lib/tomcat6/webapps/punter.war"
   owner 'root'
   group 'root'
   mode 0755
-  content ::File.open(local_file).read
-  notifies :start,  "service tomcat", :immediately
-  action :nothing
+  source src_url
+  checksum "6753cca87bec143154e8a95b0fcdcdee340352fd8977c15637336aefaa970653"
+  action :create_if_missing
+  notifies :restart,  resources(:service => "tomcat"), :immediately
 end
 
-service "tomcat" do
-  action :nothing
-end
